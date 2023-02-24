370c1b6ac8a97b8b29a6f43a444f51df034f7716
Fede <federicolucianoledesma@gmail.com>
2023-02-24 11:49:53 -0300

    Se realiza el punto 5

---
+++ js/punto5.js
@@ -0,0 +1,3 @@
+let numero1 = window.prompt('Dame un numero');
+let numero2 = window.prompt('Dame otro numero');
+document.write('La suma de los numeros ingresados es: ' + (parseInt(numero1) + parseInt(numero2)));

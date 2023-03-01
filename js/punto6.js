8bcc674ac21b782445030c6a4e0ab6df27849a8b
Fede <federicolucianoledesma@gmail.com>
2023-02-28 21:16:45 -0300

    Se agrega validacion numerica

--- js/punto6.js
+++ js/punto6.js
@@ -2,11 +2,15 @@ let numero1 = +prompt('Dame un numero');
 let numero2 = +prompt('Dame otro numero');
 
 // console.log(typeof(numero1));
-
-if(numero1 > numero2){
-    document.write(`${numero1} es mayor que ${numero2}`)
-}else if(numero1 === numero2){
-    document.write(`${numero1} es igual que ${numero2}`)
+if (!isNaN(numero1 && numero2)) {
+    if(numero1 > numero2){
+        document.write(`${numero1} es mayor que ${numero2}`)
+    }else if(numero1 === numero2){
+        document.write(`${numero1} es igual que ${numero2}`)
+    }else{
+        document.write(`${numero2} es mayor que ${numero1}`)
+    }
 }else{
-    document.write(`${numero2} es mayor que ${numero1}`)
+    alert('Ambos datos ingresados deben ser NUMERICOS!');
 }
+

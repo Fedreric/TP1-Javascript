dd4efe08b2a0811990ff179b46273e7b561d46ad
Fede <federicolucianoledesma@gmail.com>
2023-02-28 22:08:51 -0300

    Se agrega una pequeña validacion en caso de ingresar numero, pero si se ingresa una letra ya se toma como valida

--- js/punto9.js
+++ js/punto9.js
@@ -2,11 +2,16 @@ let frase = prompt('Escribe una frase, te dire cuantas vocales tiene! :D');
 let contador = 0;
 let vocales = '';
 
-for (let i = 0; i < frase.length; i++){
-    if(frase.charAt(i) == 'a' || frase.charAt(i) == 'e' || frase.charAt(i) == 'i' || frase.charAt(i) == 'o' || frase.charAt(i) == 'u' || frase.charAt(i) == 'A' || frase.charAt(i) == 'E' || frase.charAt(i) == 'I' || frase.charAt(i) == 'O' || frase.charAt(i) == 'U'){
-        vocales += frase.charAt(i);
-        contador++;
+if (isNaN(frase)){
+    for (let i = 0; i < frase.length; i++){
+        if(frase.charAt(i) == 'a' || frase.charAt(i) == 'e' || frase.charAt(i) == 'i' || frase.charAt(i) == 'o' || frase.charAt(i) == 'u' || frase.charAt(i) == 'A' || frase.charAt(i) == 'E' || frase.charAt(i) == 'I' || frase.charAt(i) == 'O' || frase.charAt(i) == 'U'){
+            vocales += frase.charAt(i);
+            contador++;
+        }
     }
+    document.write(`${frase} tiene ${contador} vocales y son ${vocales}`);
+}else{
+    document.write(`${frase} debe contener solo letas, numeros y caracteres especiales no son validos.`);
 }
 
-document.write(`${frase} tiene ${contador} vocales y son ${vocales}`);
+
